09691f91e308edd8a0ac52fdfc8c004ae9d10cd4
saintsantos <saintsantos1341@gmail.com>
2016-09-19 21:57:21 -0400

    Added a user GET endpoint to the local server

--- server/app/users/user_model.js
+++ server/app/users/user_model.js
@@ -1,6 +1,18 @@
 var db = require("app/util/db");
 
-console.log(db)
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    username: String,
+    email: String,
+    pasword: String,
+    jwt: String
+}, {collection: 'users'});
+
+var userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
 //var collection = db.get()
 
 //var collection = db.get().collection('users');
