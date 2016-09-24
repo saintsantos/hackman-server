32df6cdedf3074b2a24f62e427f72617c1c6e374
teri511 <willmartens91@gmail.com>
2016-09-24 14:07:20 -0400

    rough drafted out the addUser and addTeam commands

--- server/app/users/router.js
+++ server/app/users/router.js
@@ -4,6 +4,7 @@ var express = require('express'),
     app = express(),
 
     user = require('./user_model');
+    team = require('../teams/team_model');
 
 
 function getUser(req, res, next) {
@@ -18,6 +19,32 @@ function newUser(req, res, next) {
     //req is where all of your params are.
     //Get the info you need by calling req.query.<what you're looking for>
     //ex: password - req.qeury.password
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/hackman_proto');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+      var additionalUser = new userModel({
+          username: req.query.username,
+          email: req.query.email,
+          password: req.query.password,
+          jwt: req.query.jwt
+      });
+    });
+}
+
+function newTeam(req, res, next) {
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/hackman_proto');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+      var additionalTeam = new teamModel({
+          //req calls will go here once I look into
+          //crossreferencing info from one collection
+          //to another
+      });
+    });
 }
 
 //Just a test code for our endpoint
--- server/app/users/user_model.js
+++ server/app/users/user_model.js
@@ -5,14 +5,20 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     username: String,
-    email: String,
-    pasword: String,
+    password: String,
     jwt: String
+    grad_year: Number,
+    contact: [{
+      FB: String,
+      LI: String,
+      email: String
+    }]
 }, {collection: 'users'});
 
 var userModel = mongoose.model('User', userSchema);
 
 module.exports = userModel;
+module.exports = userSchema;
 //var collection = db.get()
 
 //var collection = db.get().collection('users');
