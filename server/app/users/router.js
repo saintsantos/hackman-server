e921d512aa2030a66e52625f8183125814a7bc61
saintsantos <saintsantos1341@gmail.com>
2016-09-24 13:01:24 -0400

    Added function where user add is supposed to go

--- server/app/users/router.js
+++ server/app/users/router.js
@@ -6,7 +6,6 @@ var express = require('express'),
     user = require('./user_model');
 
 
-
 function getUser(req, res, next) {
     //console.log(req.query);
     user.findOne({'username': req.query.username, 'password': req.query.password}, function(err, user) {
@@ -15,13 +14,15 @@ function getUser(req, res, next) {
     })
 }
 
-function sayHi(req, res, next) {
-    res.send({
-        hi: 'hi!'
-    });
+function newUser(req, res, next) {
+    //req is where all of your params are.
+    //Get the info you need by calling req.query.<what you're looking for>
+    //ex: password - req.qeury.password
 }
 
 //Just a test code for our endpoint
-router.get('/hi', getUser);
+router.get('/login', getUser);
+
+router.post('/signup', newUser)
 
 module.exports = router;
