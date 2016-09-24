9ae49cd6caa8413b3242027e348f956191753432
saintsantos <saintsantos1341@gmail.com>
2016-09-24 15:40:18 -0400

    Some people shouldn't push broken code to the dev branch
    This reverts commit 32df6cdedf3074b2a24f62e427f72617c1c6e374.

--- server/app/users/user_model.js
+++ server/app/users/user_model.js
@@ -5,20 +5,14 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     username: String,
-    password: String,
+    email: String,
+    pasword: String,
     jwt: String
-    grad_year: Number,
-    contact: [{
-      FB: String,
-      LI: String,
-      email: String
-    }]
 }, {collection: 'users'});
 
 var userModel = mongoose.model('User', userSchema);
 
 module.exports = userModel;
-module.exports = userSchema;
 //var collection = db.get()
 
 //var collection = db.get().collection('users');
