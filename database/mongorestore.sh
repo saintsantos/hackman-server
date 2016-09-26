6ed0c53f0944271d72c6a211e8f69f0b003e5801
Olivia  Alphonce <Mine@dhcp020-211-034.wireless.buffalo.edu>
2016-09-26 10:52:13 -0400

    Made mongorestore work on mac

--- database/mongorestore.sh
+++ database/mongorestore.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo mongorestore --db hackman_proto --drop ~/git/hackman-server/database/hackman_proto/
+sudo mongorestore --db hackman_proto --host=127.0.0.1 --drop ~/cse442/hackman-server/database/hackman_proto/
